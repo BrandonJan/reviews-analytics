7f631590bf71e199a71d4ec2b904cdeea3dbd175
BrandonJan <43024672+BrandonJan@users.noreply.github.com>
2021-01-26 23:21:47 +0800

    Real add filter

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -1,5 +1,18 @@
 data =[]
+count = 0
 with open("reviews.txt", "r") as f:
     for line in f:
         data.append(line)
-print(len(data))
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+print("檔案讀取完了，總共有", len(data), "筆資料")
+sum_len = 0
+for d in data:
+    sum_len += len(d)  #sum_len = sum_len + len(d)
+print("每篇留言平均長度為", sum_len/len(data))
+new =[]
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print("一共有", len(new), "筆資料字數小於100")
